cc789e0e0eca34300b0f10b50566d3651c0ef964
Drumil Patel <148728780+meetcreator@users.noreply.github.com>
2026-02-02 04:13:59 +0530

    Delete Archshield directory

--- Archshield/backend/app/api/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import login, projects, analysis
-
-api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(projects.router, prefix="/projects", tags=["projects"])
-api_router.include_router(analysis.router, prefix="/analysis", tags=["analysis"])
