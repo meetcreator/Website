aff11cd5e30377cd3fd09647c3deb34a69476819
meetcreator <imdrumil@gmail.com>
2026-04-13 21:16:27 +0530

    Update next.config.ts

--- olympiad/next.config.ts
+++ olympiad/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/olympiad'
+  basePath: process.env.NODE_ENV === 'production' ? '/olympiad' : '',
 };
 
 export default nextConfig;
